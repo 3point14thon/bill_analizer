216a933bfea20ea93c5354ef969d58d0ea22b392
3point14thon <chris.alan.bowers@gmail.com>
2019-12-10 07:28:10 -0800

    added functions to get 1st level data and pooled text body data

--- src/xml_parser.py
+++ src/xml_parser.py
@@ -4,13 +4,22 @@ import xml.etree.ElementTree as ET
 def mk_dict(bill_txt):
     root = ET.fromstring(bill_txt)
     bill = defaultdict(list)
+    bill = get_lv1_data(root.find('metadata'), bill)
+    bill = get_lv1_data(root.find('form'), bill)
+    body = 'legis_body'
+    bill[body] = get_legis_body(root.find(body)
     get_node_info(root, bill)
     return bill
 
-def get_node_info(branch, xml_dict):
-    if branch.text:
-        xml_dict[branch.tag].append(branch.text)
-    if branch.getchildren():
-        for child in branch:
-            get_node_info(child, xml_dict)
+def get_lv1_data(branch, xml_dict):
+    for datum in list(branch):
+        if datum.text:
+            bill_dict[datum.tag] = datum.text
+    return xml_dict
 
+def get_legis_body(branch, leg=''):
+    for child in branch:
+        if child.text:
+            leg = ' '.join([leg, child.text])
+        leg = par_child(child, leg)
+    return leg
