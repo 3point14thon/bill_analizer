92563fc7eca3a128aa33ad3bb16cdaf071b818a8
3point14thon <chris.alan.bowers@gmail.com>
2019-12-10 08:43:32 -0800

    added doc strings for all functions

--- src/xml_parser.py
+++ src/xml_parser.py
@@ -1,6 +1,16 @@
 import xml.etree.ElementTree as ET
 
 def mk_dict(bill_txt):
+    '''
+    Takes in bill text in xml format and produces a
+    dictionary containing most meta and form data and
+    pooled legislative body.
+
+    Args:
+        bill_txt(str): string of bill to be parsed in xml
+        format
+    Returns: Dictionary containing most of the  bills data
+    '''
     root = ET.fromstring(bill_txt)
     bill = {}
     bill = get_lv1_data(root.find('metadata'), bill)
@@ -10,12 +20,40 @@ def mk_dict(bill_txt):
     return bill
 
 def get_lv1_data(branch, xml_dict):
+    '''
+    returns a dictionary of data extracted from the children
+    of the given branch but not the childrens "decendents".
+
+    Args:
+        branch(xml_element): Parent of elements data is to
+        be extracted from.
+
+        xml_dict(dict): Dictionary extracted data is loaded
+        into.
+
+    Returns: Dictionary of extracted data.
+    '''
     for child in list(branch):
         if child.text:
             xml_dict[child.tag] = child.text
     return xml_dict
 
 def get_legis_body(branch, leg=''):
+    '''
+    returns a string of all text fields in all downstream
+    elements from the given branch concatinated into one
+    string.
+
+    Args:
+        branch(xml_element): root element used to find all
+        downstream elements.
+
+        leg(str): kernal string added to to form pooled text
+        for all elements in given tree.
+
+    Returns: String of pooled text from all text fields in
+    given tree.
+    '''
     for child in list(branch):
         if child.text:
             leg = ' '.join([leg, child.text])
