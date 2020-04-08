41fa91aa6eb0e9df03ff3c60c0e75afc9a5780e0
3point14thon <chris.alan.bowers@gmail.com>
2020-04-08 09:51:36 -0700

    changed function so the max and min numeric values are inputs

--- src/html_cscale.py
+++ src/html_cscale.py
@@ -1,12 +1,14 @@
-def c_scale(num_val, c_max=255):
+def c_scale(num_val, lower_bound = 0 ,upper_bound = 1):
     '''
     Returns a tuble of rgb values and opacity based on the inputs.
 
     Inputs:
         num _val (float): The value of this instance on the scale.
-        c_max (int): Determins how quickly the scale increases.
+        lower_bound (float):The lowest value on the numeric scale.
+        upper_bound (float): The highest value on the numeric scale.
 
     Outputs: Tuple of rgba values
     '''
-    c_value = num_val*c_max
+    c_max=255
+    c_value = ((num_val-lower_bound)/(upper_bound-lower_bound))*c_max
     return (c_max - c_value, c_value, 0, 0.2)
