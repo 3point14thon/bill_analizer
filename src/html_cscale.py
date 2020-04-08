c0fc4489f58a4334707c2ae000e0b917c7175ba7
3point14thon <chris.alan.bowers@gmail.com>
2020-04-08 10:16:32 -0700

    added a function that makes colored spans given test and vals

--- src/html_cscale.py
+++ src/html_cscale.py
@@ -1,4 +1,13 @@
-def c_scale(num_val, lower_bound = 0 ,upper_bound = 1):
+def mk_color_span(vals, txts, lower_bound=0, upper_bound=1):
+    span_start = '<span style="background-color:rgba'
+    color_doc = [span_start +
+                 f'{c_scale(val, lower_bound, upper_bound)};">' +
+                 str(txt) +
+                 '</span>' for txt, val in zip(txts, vals)]
+    return ''.join(color_doc)
+
+
+def c_scale(num_val, lower_bound=0, upper_bound=1):
     '''
     Returns a tuble of rgb values and opacity based on the inputs.
 
@@ -9,6 +18,6 @@ def c_scale(num_val, lower_bound = 0 ,upper_bound = 1):
 
     Outputs: Tuple of rgba values
     '''
-    c_max=255
+    c_max = 255
     c_value = ((num_val-lower_bound)/(upper_bound-lower_bound))*c_max
     return (c_max - c_value, c_value, 0, 0.2)
