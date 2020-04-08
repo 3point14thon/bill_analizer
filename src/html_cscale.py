c8159416b4eb6257ae22eb08806d6f30a2345a42
3point14thon <chris.alan.bowers@gmail.com>
2020-04-08 09:37:37 -0700

    add doc string

--- src/html_cscale.py
+++ src/html_cscale.py
@@ -1,3 +1,12 @@
 def c_scale(num_val, c_max=255):
+    '''
+    Returns a tuble of rgb values and opacity based on the inputs.
+
+    Inputs:
+        num _val (float): The value of this instance on the scale.
+        c_max (int): Determins how quickly the scale increases.
+
+    Outputs: Tuple of rgba values
+    '''
     c_value = num_val*c_max
     return (c_max - c_value, c_value, 0, 0.2)
